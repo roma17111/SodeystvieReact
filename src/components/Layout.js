50f794c9ee28b94e59dbca91ad798ad06ba5a1f9
roman <romanze1706@gmail.com>
2023-07-30 18:08:21 +0300

    ...

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link, Outlet} from "react-router-dom";
 import './Layout.css'
 import logo from "../photos/sod.jpg";
+import {HeaderInfo} from "./header/HeaderInfo";
 
 let date = new Date().getFullYear()
 
@@ -9,7 +10,8 @@ const Layout = () => {
     return (
         <div className="wrapper">
             <header className="header">
-                <img src={logo} className="main-image" alt="Картинка"/>
+                <img src={logo} className="main-image container-md m-lg-3" alt="Картинка"/>
+                <HeaderInfo/>
             </header>
             <main className="main">
                 <Outlet/>
--- src/components/header/HeaderInfo.js
+++ src/components/header/HeaderInfo.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './HeaderInfo.css'
 const HeaderInfo = () => {
     return (
-        <div>
-
+        <div className="info-div-header">
+            <h5 className='container-md'>РЕГИОНАЛЬНАЯ ОБЩЕСТВЕННАЯ ОРГАНИЗАЦИЯ ИНВАЛИДОВ</h5>
         </div>
     );
 };
