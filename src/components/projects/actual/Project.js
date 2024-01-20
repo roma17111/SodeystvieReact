441f8b3306cf87ad32c00a4e4350fb66c0caf2c1
yakimenko <r.yakimenko@pandev.kz>
2024-01-20 09:08:56 +0300

    Исправлены ошибки вёрстки

--- src/components/projects/actual/Project.js
+++ src/components/projects/actual/Project.js
@@ -3,13 +3,13 @@ import "./project.css"
 
 const Project = (props) => {
     return (
-        <div className="m-5">
+        <div className="m-auto">
             <div className="lager-text">
                 <h2 className="text-center">{props.el.name}</h2>
                 <h3 className="text-center">{props.el.description}</h3>
             </div>
             <div className="p-5">
-            <img className="lager-image img-fluid rounded mx-auto d-block " src={props.el.image} alt="lager" width="650"/>
+            <img className="project-image  " src={props.el.image} alt="lager" width="650"/>
             </div>
             <div >
                 <span className="text text-center">{props.el.text}</span>
