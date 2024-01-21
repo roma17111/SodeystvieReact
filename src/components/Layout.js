c7e46e514551058b9a957cebe681fbe2da6c71e0
yakimenko <r.yakimenko@pandev.kz>
2024-01-21 10:16:53 +0300

    Footer.js перенесён в отдельный компонент.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import {Link, Outlet} from "react-router-dom";
 import './Layout.css'
 
 import {HeaderInfo} from "./header/HeaderInfo";
+import Footer from "./Footer";
 
 let date = new Date().getFullYear()
 
@@ -16,18 +17,7 @@ const Layout = () => {
                 <Outlet/>
             </main>
             <footer className="footer p-2">
-                <div className="container-fluid text-center text-footer-color">
-                    <span className="text-center">©2004-{date} Все права защищены</span>
-                </div>
-                <div className="text-center">
-                    <p><a className="text-center a-footer text-footer-color" href="mailto:sodeystvie@gmail.com">📧 sodeystvie@gmail.com</a></p>
-                </div>
-                <div className="text-center mb-2 text-footer-color">
-                    <span>📞 +7(916)157-11-31</span>
-                </div>
-                <div className="created text-center text-footer-color">
-                    <span>Created by Roman and Nika</span>
-                </div>
+               <Footer/>
             </footer>
         </div>
     );
